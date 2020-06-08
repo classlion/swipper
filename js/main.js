1bb6ddb10556bbca9255546e646c471a95820b8c
classlion <classlion1@gmail.com>
2020-06-08 18:22:07 +0300

    First

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+var swiper = new Swiper('.secodary-swiper-container', {
+    pagination: {
+        el: '.secodary-swiper-pagination',
+    },
+});
